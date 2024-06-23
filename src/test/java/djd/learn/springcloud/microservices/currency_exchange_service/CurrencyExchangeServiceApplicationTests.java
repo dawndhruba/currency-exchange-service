ccd4eadf28ef7d59c548b53c6aae233a968e279c
Dhruba Dawndawndhruba@gmail.com <dawndhruba@gmail.com>
2024-06-23 12:46:49 +0530

    First commit

---
+++ src/test/java/djd/learn/springcloud/microservices/currency_exchange_service/CurrencyExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package djd.learn.springcloud.microservices.currency_exchange_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
